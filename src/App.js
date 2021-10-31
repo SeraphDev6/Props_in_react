dcf30648dbf6f32bba43c6d633183c7df054f617
SeraphDev6 <wesley.giles@gmail.com>
2021-10-31 01:32:24 -0400

    inital commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import './App.css';
+import PersonCard from './components/perconCard'
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+      <PersonCard firstName = "Wesley" lastName = "Giles" age={25} hairColor = "Silver" />
+      <PersonCard firstName = "Blargity" lastName = "Blarg" age={5555} hairColor = "Blarg" />
+      <PersonCard firstName = "Katie" lastName = "Posey" age={26} hairColor = "Red" />
+      <PersonCard firstName = "Captain" lastName = "Crunch" age={58} hairColor = "White" />
+      </header>
+    </div>
+  );
+}
+
+export default App;
