a5c36055e2920cdf8b3cb3278e428ccd09132909
SeraphDev6 <wesley.giles@gmail.com>
2021-10-31 01:45:56 -0400

    adde bithday button

--- src/components/perconCard.js
+++ src/components/perconCard.js
@@ -1,13 +1,23 @@
 import React from 'react';
 
 class PersonCard extends React.Component{
+    constructor(props){
+        super(props);
+        this.state={
+            age:this.props.age
+        };
+    }
+    happyBirthday = () => {
+        this.setState({age:(this.state.age+1)});
+    }
     render(){
-        const {firstName,lastName,age,hairColor}=this.props;
+        const {firstName,lastName,hairColor}=this.props;
         return(
             <div>
             <h2>{lastName}, {firstName}</h2>
-            <p>Age : {age}</p>
+            <p>Age : {this.state.age}</p>
             <p>Hair Color : {hairColor}</p>
+            <button onClick={this.happyBirthday}>Happy Birthday {firstName} {lastName}!!</button>
             </div>
         )
     }
