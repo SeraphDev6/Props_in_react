6cfd01b0a6782a95926b55820c54f3bc722e609d
SeraphDev6 <wesley.giles@gmail.com>
2021-11-04 00:30:32 -0400

    modularized PersonCard back into original file

--- src/components/personCard.js
+++ src/components/personCard.js
@@ -1,26 +1,36 @@
 import React from 'react';
 
-class PersonCard extends React.Component{
-    constructor(props){
-        super(props);
-        this.state={
-            age:this.props.age
-        };
-    }
-    happyBirthday = () => {
-        this.setState({age:(this.state.age+1)});
-    }
-    render(){
-        const {firstName,lastName,hairColor}=this.props;
-        return(
-            <div>
-            <h2>{lastName}, {firstName}</h2>
-            <p>Age : {this.state.age}</p>
-            <p>Hair Color : {hairColor}</p>
-            <button onClick={this.happyBirthday}>Happy Birthday {firstName} {lastName}!!</button>
-            </div>
-        )
-    }
+// class PersonCard extends React.Component{
+//     constructor(props){
+//         super(props);
+//         this.state={
+//             age:this.props.age
+//         };
+//     }
+//     happyBirthday = () => {
+//         this.setState({age:(this.state.age+1)});
+//     }
+//     render(){
+//         const {firstName,lastName,hairColor}=this.props;
+//         return(
+//             <div>
+//             <h2>{lastName}, {firstName}</h2>
+//             <p>Age : {this.state.age}</p>
+//             <p>Hair Color : {hairColor}</p>
+//             <button onClick={this.happyBirthday}>Happy Birthday {firstName} {lastName}!!</button>
+//             </div>
+//         )
+//     }
+// }
+
+const PersonCard = props => {
+    return(
+    <div>
+    <h2>{props.lastName}, {props.firstName}</h2>
+    <p>Age : {props.age}</p>
+    <p>Hair Color : {props.hairColor}</p>
+    </div>
+    )
 }
 
 export default PersonCard
